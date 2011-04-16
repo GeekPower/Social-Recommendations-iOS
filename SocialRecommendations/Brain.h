2b4ba9035c6ff0069b36938ccab0d917eeaf82e2
Luiza Cicone <luiza.cicone@gmail.com>
2011-04-16 14:17:44 +0300

    Brain of app added

---
+++ SocialRecommendations/Brain.h
@@ -0,0 +1,16 @@
+//
+//  Brain.h
+//  SocialRecommendations
+//
+//  Created by Luiza Cicone on 4/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Brain : NSObject {
+    
+}
+
+@end
---
+++ SocialRecommendations/Brain.m
@@ -0,0 +1,14 @@
+//
+//  Brain.m
+//  SocialRecommendations
+//
+//  Created by Luiza Cicone on 4/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Brain.h"
+
+
+@implementation Brain
+
+@end
